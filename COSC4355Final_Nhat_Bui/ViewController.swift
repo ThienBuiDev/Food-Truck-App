97d45409359bebd76fc933a95b3663be36b04fd4
David Bui <tbui.vdev@gmail.com>
2021-10-24 22:09:44 -0500

    Initial Commit

---
+++ COSC4355Final_Nhat_Bui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COSC4355Final_Nhat_Bui
+//
+//  Created by Thien Bui on 10/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COSC4355Final_Nhat_BuiTests/COSC4355Final_Nhat_BuiTests.swift
@@ -0,0 +1,33 @@
+//
+//  COSC4355Final_Nhat_BuiTests.swift
+//  COSC4355Final_Nhat_BuiTests
+//
+//  Created by Thien Bui on 10/24/21.
+//
+
+import XCTest
+@testable import COSC4355Final_Nhat_Bui
+
+class COSC4355Final_Nhat_BuiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
